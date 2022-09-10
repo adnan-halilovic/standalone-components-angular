595c895f9f866db005706910237ff682191c107e
Adnan Halilovic <adnan.halilovic@mail.com>
2022-09-10 17:00:33 +0200

    Standalone Components in Angular - Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DefaultComponent } from './default/default.component';
+import { StandaloneComponent2 } from './standalone copy/standalone.component';
+import { StandaloneComponent } from './standalone/standalone.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, DefaultComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StandaloneComponent,
+    StandaloneComponent2,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
